d8b23ce15cce28b788143941a782724f376643c0
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-21 00:39:31 +0200

    adds cropped images to genre view

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`, //w:1024<
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
--- game-hub/src/compontents/GameGrid.tsx
+++ game-hub/src/compontents/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         padding="10px"
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
