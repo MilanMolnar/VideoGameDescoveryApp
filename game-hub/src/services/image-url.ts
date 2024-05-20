ef9f494dcd35c327382f5f16d19ad2c1d46e37f5
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-20 19:38:29 +0200

    implements cropped urls for faster load times with apis on the flight image cropping api

---
+++ game-hub/src/services/image-url.ts
@@ -0,0 +1,15 @@
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const resolution = {
+    width: 600,
+    height: 400,
+  };
+  const index = url.indexOf(target) + target.length;
+  return (
+    url.slice(0, index) +
+    `crop/${resolution.width}/${resolution.height}/` +
+    url.slice(index)
+  );
+};
+
+export default getCroppedImageUrl;
