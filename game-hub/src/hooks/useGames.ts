9164d00b2f9f012104fd489589ae783a4625236a
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-20 17:57:00 +0200

    Segregate fetching games to a  custom hook

---
+++ game-hub/src/hooks/useGames.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface fetchGameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const contorller = new AbortController();
+
+    apiClient
+      .get<fetchGameResponse>("/games", { signal: contorller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    return () => contorller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
