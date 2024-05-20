79b90c837e57e723f31cb609bac4843fa425a27c
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-20 19:26:31 +0200

    implements a metacritic score badge with the data coming from our backend and sets the color scheme based on its value

---
+++ game-hub/src/compontents/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color =
+    score > 80 ? "green" : score > 60 ? "yellow" : score > 30 ? "" : "red";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
