ff35f41353a3c499bbc7c39f58e825559c1ea66d
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-21 01:18:01 +0200

    highlights selected genre with bold style

--- game-hub/src/compontents/GenreList.tsx
+++ game-hub/src/compontents/GenreList.tsx
@@ -7,9 +7,10 @@ import { Genre } from "../hooks/useGenres";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -25,6 +26,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize={"lg"}
               variant="link"
