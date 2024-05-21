09c78cf7093df9e9d8009945b78dc1d05829fba5
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-21 17:28:08 +0200

    handles gamecards with no image

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const resolution = {
     width: 600,
