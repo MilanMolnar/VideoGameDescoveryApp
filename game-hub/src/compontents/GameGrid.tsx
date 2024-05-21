bfbc51a64586a58e5c6717e5678cce8c466c21a6
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-21 18:43:03 +0200

    Refactor: gamegrid error handling

--- game-hub/src/compontents/GameGrid.tsx
+++ game-hub/src/compontents/GameGrid.tsx
@@ -13,28 +13,25 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        padding="10px"
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={5}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game}></GameCard>{" "}
+    <SimpleGrid
+      padding="10px"
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={5}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game}></GameCard>{" "}
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
