70f963b8d792d30028f7e6ac58eb161200612f25
Molnar Milan <molnarmilan.cc@gmail.com>
2024-05-21 18:40:43 +0200

    changes theme to a darker set of colors

--- game-hub/src/theme.ts
+++ game-hub/src/theme.ts
@@ -6,6 +6,20 @@ const config: ThemeConfig = {
 
 const theme = extendTheme({
   config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#a0a0a0",
+      500: "#898989",
+      600: "#636363",
+      700: "#202020",
+      800: "#121212",
+      900: "#111",
+    },
+  },
 });
 
 export default theme;
