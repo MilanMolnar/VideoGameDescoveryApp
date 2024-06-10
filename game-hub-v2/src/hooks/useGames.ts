7c39e1f98e26dc2a327d0b45814b00f7641286c2
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-10 20:06:52 +0200

    simplifies time calc with ms lib

--- game-hub-v2/src/hooks/useGames.ts
+++ game-hub-v2/src/hooks/useGames.ts
@@ -1,8 +1,9 @@
 import APIClient from "../services/api-client";
 import { GameQuery } from "../App";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -30,7 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
   });
 
 export default useGames;
--- game-hub-v2/src/hooks/useGenres.ts
+++ game-hub-v2/src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
+import ms from "ms";
 
 export interface Genre {
   id: number;
@@ -14,7 +15,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: genres,
   });
 
