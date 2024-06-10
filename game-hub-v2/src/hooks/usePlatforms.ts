25a84b18f417131e77fa5375cd8fe0f8d0cbda3c
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-10 16:28:05 +0200

    refactoring, creates a reusable api client with axios

--- game-hub-v2/src/hooks/usePlatforms.ts
+++ game-hub-v2/src/hooks/usePlatforms.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { FetchResponse } from "../services/api-client";
-import apiClient from "../services/api-client";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -11,10 +13,7 @@ export interface Platform {
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Platform>>("/platforms/lists/parents")
-        .then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
   });
 
--- game-hub-v2/src/services/api-client.ts
+++ game-hub-v2/src/services/api-client.ts
@@ -1,13 +1,28 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "7ff3dac2861a47b2ab90c00a07b3a275",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
