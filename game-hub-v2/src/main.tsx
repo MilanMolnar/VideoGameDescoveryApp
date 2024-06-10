5d16ed28eec376c0716a966338f22014c929ad78
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-10 13:12:42 +0200

    installs reactQuery and refactors useGenres cutsom hook to use useQueryClient

--- game-hub-v2/src/main.tsx
+++ game-hub-v2/src/main.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 import theme from "./theme";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider theme={theme}>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </ChakraProvider>
 );
